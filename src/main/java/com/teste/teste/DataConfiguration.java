c9a9032f34db4711b0d2a67ffda04df497759a9e
Hélio José <helinhojnior78@gmail.com>
2020-09-22 20:39:41 +0200

    Versão 0.1

---
+++ src/main/java/com/teste/teste/DataConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teste.teste;
+
+import javax.activation.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ *
+ * @author User
+ */
+@Configuration
+public class DataConfiguration {
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/reservas");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//        return (DataSource) dataSource;
+//    }
+
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+//        dataSourceBuilder.url("jdbc:mysql://localhost:3306/reservas");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("");
+//        return (DataSource) dataSourceBuilder.build();
+//    }
+}
