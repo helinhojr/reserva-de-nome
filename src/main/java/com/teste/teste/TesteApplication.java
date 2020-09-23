f5881478c7560e70091951cbabfa974fa6daaf44
Hélio José <helinhojnior78@gmail.com>
2020-09-23 10:07:48 +0200

    Versao

--- src/main/java/com/teste/teste/TesteApplication.java
+++ src/main/java/com/teste/teste/TesteApplication.java
@@ -2,12 +2,18 @@ package com.teste.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TesteApplication {
+public class TesteApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TesteApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(TesteApplication.class);
+    }
 }
